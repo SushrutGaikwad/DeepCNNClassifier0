c45c166b2aee42da82372b1053064f3c4d3b2538
Sushrut Gaikwad <sushrut.ishwargaikwad@students.iiserpune.ac.in>
2023-06-30 17:38:47 +0530

    Add data ingestion pipeline

---
+++ src/DeepClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,26 @@
+from DeepClassifier.config import ConfigurationManager
+from DeepClassifier.components import DataIngestion
+from DeepClassifier import logger
+
+
+STAGE_NAME = "Data Ingestion"
+
+
+def main():
+    config = ConfigurationManager()
+
+    data_ingestion_config = config.get_data_ingestion_config()
+
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_data_file()
+    data_ingestion.unzip_and_clean_data_file()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>>>>>>> {STAGE_NAME} Stage Started <<<<<<<<<<<<")
+        main()
+        logger.info(f">>>>>>>>>>>> {STAGE_NAME} Stage Completed <<<<<<<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
